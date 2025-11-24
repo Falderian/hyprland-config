fb27d446105c0055d4d4cec14ffe5d811a3d74b9
falderian <falderian>
2025-11-24 19:44:50 +0300

    2025-11-24 19:44:50

--- install-config.sh
+++ install-config.sh
@@ -2,7 +2,7 @@
 
 # Define repo path and config directories
 REPO_PATH="./"
-CONFIG_ENTRIES=("swaync" "htop" "hypr" "foot" "ml4w" "wofi" "waybar" "starship.toml" "gtk-3.0" "nwg-look" "xsettingsd")
+CONFIG_ENTRIES=("swaync" "htop" "hypr" "kitty" "ml4w" "wofi" "waybar" "starship.toml")
 
 # Define required packages
 PKG_LIST=("swaync" "htop" "hyprland" "foot" "wofi" "waybar" "starship" "gtk3" "nwg-look" "xsettingsd")
